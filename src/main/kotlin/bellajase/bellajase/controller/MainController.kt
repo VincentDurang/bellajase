85590db3b6442fa0a346b7a8b83dbd888e8b5c45
Durang <durangvincent0gmail.com>
2023-02-07 20:50:15 +0100

    systeme d'enregistremnt de contact

--- src/main/kotlin/bellajase/bellajase/controller/MainController.kt
+++ src/main/kotlin/bellajase/bellajase/controller/MainController.kt
@@ -1,12 +1,19 @@
 package bellajase.bellajase.controller
 
+import bellajase.bellajase.dto.ContactDTO
+import bellajase.bellajase.repository.ContactRepository
+import bellajase.bellajase.toContact
+import jakarta.validation.Valid
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
+import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 
 @Controller
-class MainController {
+class MainController @Autowired constructor( private val contactRepository: ContactRepository) {
 
     @GetMapping("/")
     fun main(model: Model): String {
@@ -25,7 +32,17 @@ class MainController {
     }
 
     @GetMapping("/contact")
-    fun contact(model: Model): String {
+    fun contact(contactDTO: ContactDTO): String {
         return "contact"
+    }
+
+    @PostMapping("/contact")
+    fun addContact(@Valid contactDTO: ContactDTO, bindingResult: BindingResult): String {
+        return if (bindingResult.hasErrors()) {
+            "accueil"
+        } else {
+            contactRepository.save(contactDTO.toContact())
+            "accueil"
+        }
     }
 }
